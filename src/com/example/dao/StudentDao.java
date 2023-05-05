de6df59da186cce93797470830da4045ddb804ab
rtok <rtok@DSs-MacBook-Pro.fritz.box>
2023-05-05 12:19:29 +0200

    initial commit

--- src/com/example/dao/StudentDao.java
+++ src/com/example/dao/StudentDao.java
@@ -1,4 +1,37 @@
 package com.example.dao;
-public class StudentDao {
+import com.example.model.Student;
+
+public class StudentDao implements StudentDaoInterface{
+
+    @Override
+    public boolean insertStudent(Student s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertStudent'");
+    }
+
+    @Override
+    public boolean delete(int roll) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public boolean update(int roll, String update, int ch, Student s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void showAllStudent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showAllStudent'");
+    }
+
+    @Override
+    public boolean showStudentById(int roll) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showStudentById'");
+    }
+
     
 }
